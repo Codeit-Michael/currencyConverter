34233a3009d34f31277c5a8e53031f8d5088667d
Michael Maranan <michaelmaranan030@gmail.com>
2021-09-15 08:45:05 +0800

    startting the project all over again

--- converter/functions.py
+++
@@ -1,8 +0,0 @@
-def convertion_list(lst=list):
-	# takes a list of object and convert it into tuples
-	new_list = []
-	for x in lst:
-		tp = (x,x)
-		new_list.append(tp)
-	return new_list
-
--- converter/views.py
+++ converter/views.py
@@ -1,33 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import *
+import requests
 
 # Create your views here.
-def hello(request):
-	form = converterForm
-	if request.method == 'POST':
-		amount = request.POST['amount']
-		my_currency = request.POST['my_currency']
-		to_currency = request.POST['to_currency']
-
-		if my_currency != 'EUR':
-			amount_in_euro = int(amount) / rates.get(my_currency)
-			new_amount = amount_in_euro * rates.get(to_currency)
-
-		elif str(my_currency) == 'EUR':
-			new_amount = int(amount) * rates.get(to_currency)
-
-		my_currency_symbol = CurrencySymbols.get_symbol(my_currency)
-		to_currency_symbol = CurrencySymbols.get_symbol(to_currency)
-
-		my_dict = {
-			'new_amount': new_amount,
-			'amount': amount,
-			'my_currency_symbol': my_currency_symbol,
-			'to_currency_symbol': to_currency_symbol,
-		}
-
-		return render(request, 'converter/ans.html', my_dict)
-
-	else:
-	    return render(request, 'converter/home.html', {'form':form})
+def cur(request):
+	response = requests.get("https://api.exchangerate.host/latest")
