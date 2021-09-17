fbda46eb812d9c963dee62ddd15789a27b974622
Michael Maranan <michaelmaranan030@gmail.com>
2021-09-17 16:46:21 +0800

    add form

---
+++ converter/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+import requests
+
+response = requests.get("https://api.exchangerate.host/latest")
+currency = response.json()
+
+CURRENCY_CHOICE = []
+
+for x in currency['rates']:
+	CURRENCY_CHOICE.append((x,x))
+
+class converterForm(forms.Form):
+	amount = forms.IntegerField()
+	myCurrency = forms.ChoiceField(choices=CURRENCY_CHOICE)
+	toCurrency = forms.ChoiceField(choices=CURRENCY_CHOICE)
--- converter/views.py
+++ converter/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from .forms import converterForm
 
 # Create your views here.
 def convert(request):
+	myForm = converterForm()
 	response = requests.get("https://api.exchangerate.host/latest")
 	myJson = response.json()
 	currs = myJson['rates']
 
-	return render(request,'converter/home.html',{'currs':currs})
+	return render(request,'converter/home.html',{'currs':currs, 'myForm':myForm})
